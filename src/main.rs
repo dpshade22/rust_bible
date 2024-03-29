641f7dc80f7a856167c53a5736d39ae6a2f7dd9a
Dylan Shade <dylanpriceshade@icloud.com>
2024-03-29 17:31:57 -0400

    clean UI checkpoint. Need chapter selector still

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod verse;
 
 use dioxus::prelude::*;
 use log::debug;
-use verse::{fetch_verses_from_url, Bible};
+use verse::*;
 
 fn main() {
     // Urls are relative to your Cargo.toml file
@@ -21,6 +21,7 @@ fn App() -> Element {
     let mut current_chapter = use_signal(|| "".to_string());
     let mut current_chapter_text = use_signal(|| "".to_string());
     let mut unique_books = use_signal(|| vec![]);
+    let mut chapter_tuples = use_signal(|| Vec::new());
 
     // let mut search_keyword = use_signal(|| "".to_string());
 
@@ -60,64 +61,59 @@ fn App() -> Element {
                     }),
             );
             unique_books.set(fetched_bible.get_unique_books());
+
+            let chapter_tuples_vec: Vec<(String, Chapter)> = fetched_bible
+                .chapters
+                .iter()
+                .map(|chapter| (chapter.r#ref.clone(), chapter.clone()))
+                .collect();
+            chapter_tuples.set(chapter_tuples_vec);
         }
     });
 
     rsx! {
-    div {
-        class: "flex w-full bg-gray-100/40",
-        display: "flex",
-        flex_direction: "row",
         div {
-            class: "flex flex-col  border-r bg-gray-100/40 w-[250px] lg:block max-h-screen overflow-y-auto ",
-            nav {
-                class: "flex flex-col flex-1",
-                // div {
-                //     class: "py-2 md:py-4 lg:py-2",
-                //     h1 {
-                //         class: "flex justify-center items-center space-x-4 text-xl font-semibold",
-                //         "Books"
-                //     }
-                //     hr {}
-                // }
-                div {
-                    class: "flex-1 grid items-start px-4 py-2 text-sm font-medium",
-                    if let Some(curr_bible) = bible() {
-                        for book in unique_books() {
-                            button {
-                                class: "block px-4 py-2 rounded-md hover:bg-gray-200",
-                                onclick: move |_| {
-                                    match bible() {
-                                        Some(mut curr_bible) => {
-                                            debug!("{}", &book);
-                                            let chapter_ref = curr_bible.chapters
-                                                .iter()
-                                                .find(|chapter| &chapter.book == &book)
-                                                .map(|chapter| chapter.r#ref.as_str())
-                                                .map(|s| s.to_string());
+            class: "flex w-full bg-gray-100/40",
+            display: "flex",
+            flex_direction: "row",
+            div {
+                class: "flex border-r bg-gray-100/40 w-[250px] lg:block max-h-screen overflow-y-auto",
+                nav {
+                    div {
+                        class: "flex-1 grid items-start px-4 py-2 text-sm font-medium",
+                        if let Some(curr_bible) = bible() {
+                            for book in unique_books() {
+                                button {
+                                    class: "py-2",
+                                    onclick: move |_| {
+                                        match bible() {
+                                            Some(mut curr_bible) => {
+                                                debug!("{}", &book);
+                                                let chapter_ref = curr_bible.chapters
+                                                    .iter()
+                                                    .find(|chapter| &chapter.book == &book)
+                                                    .map(|chapter| chapter.r#ref.as_str())
+                                                    .map(|s| s.to_string());
 
-                                            if let Some(chapter_ref) = chapter_ref {
-                                                curr_bible.go_to_chapter(&chapter_ref);
-                                                current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
-                                                current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
-                                                bible.set(Some(curr_bible));
+                                                if let Some(chapter_ref) = chapter_ref {
+                                                    curr_bible.go_to_chapter(&chapter_ref);
+                                                    current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
+                                                    current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
+                                                    bible.set(Some(curr_bible));
+                                                }
                                             }
+                                            None => {debug!("{}", book);}
                                         }
-                                        None => {debug!("{}", book);}
-                                    }
-                                },
-                                // "{book}"
-
+                                    },
                                     if curr_bible.get_current_chapter().map_or(false, |chapter| chapter.book == book) {
-
                                         strong {
-                                            class: "block text-3xl px-4 py-2 rounded-md bg-slate-700	",
+                                            class: "flex text-lg px-4 py-2 rounded-md text-white bg-gray-700",
                                             "{book.to_uppercase()}"
                                         }
                                     } else {
                                         "{book}"
                                     }
-                            }
+                                }
                             }
                         }
                     },
@@ -126,90 +122,156 @@ fn App() -> Element {
             div {
                 class: "flex-1 max-h-screen overflow-y-auto",
                 div {
-                    class: "px-4 py-6 md:px-6 md:py-12 lg:py-16",
+                    class: "flex px-4 pt-2",
                     div {
-                        class: "space-y-6 prose prose-gray max-w-6xl mx-auto",
+                        class: "flex py-6 items-center w-full",
+
+                        button {
+                            class: "text-gray-500 hover:text-gray-700 order-1",
+                            onclick: move |_| {
+                                match bible() {
+                                    Some(mut curr_bible) => {
+                                        curr_bible.previous_chapter();
+                                        current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
+                                        current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
+                                        bible.set(Some(curr_bible));
+                                    },
+                                    None => debug!("Bible match failed")
+                                }
+                            },
+                            svg {
+                                class: "h-6 w-6",
+                                fill: "none",
+                                view_box: "0 0 24 24",
+                                stroke: "currentColor",
+                                path {
+                                    stroke_linecap: "round",
+                                    stroke_linejoin: "round",
+                                    stroke_width: "2",
+                                    d: "M15 19l-7-7 7-7",
+                                }
+                            }
+                        }
                         h1 {
-                            class: "text-4xl font-extrabold tracking-tight lg:text-5xl mb-4 inline-block",
+                            class: "text-4xl font-extrabold tracking-tight lg:text-5xl mx-4 w-50% truncate order-2",
                             "{current_chapter}"
                         }
-                        div {
-                            class: "flex space-x-4 overflow-x-auto pb-4 scrollbar-hide",
-                            if let Some(curr_bible) = bible() {
-                                for chapter in curr_bible.chapters {
-                                    button {
-                                        class: "flex-none rounded-md px-4 py-2 text-sm font-medium hover:bg-gray-200 focus:outline-none",
-                                        class: if chapter.get_pretty_chapter() == current_chapter() { "bg-slate-700 text-white" } else { "bg-white" },
-                                        onclick: move |_| {
-                                            current_chapter_text.set(chapter.text.clone());
-                                            current_chapter.set(chapter.get_pretty_chapter());
-                                        },
-                                        "{chapter.get_pretty_chapter()}"
-                                    }
+                        button {
+                            class: "text-gray-500 hover:text-gray-700 order-3",
+                            onclick: move |_| {
+                                match bible() {
+                                    Some(mut curr_bible) => {
+                                        curr_bible.next_chapter();
+                                        current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
+                                        current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
+                                        bible.set(Some(curr_bible));
+                                    },
+                                    None => debug!("Bible match failed")
                                 }
-                            }
-                            }
-                        hr {}
-                        div {
-                            class: "space-y-4 prose prose-gray max-w-prose",
-
-                            if let Some(curr_bible) = bible() {
-                                if let Some(chapter) = curr_bible.get_current_chapter() {
-                                    {
-                                        chapter.verses.iter().map(|verse| {
-                                            rsx! {
-                                                p {
-                                                    class: "text-lg leading-loose",
-                                                    dangerous_inner_html: format_args!("<b>{}</b> {} <br />", verse.verse_num, verse.text),
-                                                    " "
-                                                }
-                                            }
-                                        })
-                                    }
+                            },
+                            svg {
+                                class: "h-6 w-6",
+                                fill: "none",
+                                view_box: "0 0 24 24",
+                                stroke: "currentColor",
+                                path {
+                                    stroke_linecap: "round",
+                                    stroke_linejoin: "round",
+                                    stroke_width: "2",
+                                    d: "M9 5l7 7-7 7",
                                 }
                             }
                         }
                     }
                 }
+                // div {
+                //     class: "flex space-x-4 overflow-x-auto pb-4 scrollbar-hide",
+                //     if let Some(curr_bible) = bible() {
+                //         select {
+                //             class: "block w-2 rounded-md shadow-none bg-slate-50",
+                //             onchange: move |evt: Event<FormData>| {
+                //                 let selected_ref = evt.value();
+                //                 if let Some(mut new_bible) = bible() {
+                //                     if let Some(chapter) = new_bible.chapters.iter().find(|ch| ch.r#ref == selected_ref) {
+                //                         current_chapter_text.set(chapter.text.clone());
+                //                         current_chapter.set(chapter.get_pretty_chapter());
+                //                         new_bible.go_to_chapter(&selected_ref);
+                //                         bible.set(Some(new_bible));
+                //                     }
+                //                 }
+                //             },
+                //             {curr_bible.chapters.iter()
+                //                 .filter(|chapter| chapter.book == curr_bible.get_current_chapter().map_or("".to_string(), |curr_chapter| curr_chapter.book.clone()))
+                //                 .map(|chapter| rsx! {
+                //                     option {
+                //                         value: "{chapter.r#ref}",
+                //                         selected: chapter.get_pretty_chapter() == current_chapter(),
+                //                         "Ch. {chapter.chapter}"
+                //                     }
+                //                 })}
+                //         }
+                //     }
+                // }
+
+                hr {}
                 div {
-                    class: "fixed bottom-0 left-250 m-4",
-                    button {
-                        class: "px-4 py-2 text-black bg-slate-300 rounded",
-                        onclick: move |_| {
-                            match bible() {
-                                Some(mut curr_bible) => {
-                                    curr_bible.previous_chapter();
-                                    debug!("{:?}", current_chapter);
-                                    current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
-                                    current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
-                                    bible.set(Some(curr_bible));
-                                },
-                                None => debug!("Bible match failed")
+                    class: "mx-4 my-4 prose-gray max-w-prose",
+
+                    if let Some(curr_bible) = bible() {
+                        if let Some(chapter) = curr_bible.get_current_chapter() {
+                            {
+                                chapter.verses.iter().map(|verse| {
+                                    rsx! {
+                                        p {
+                                            class: " leading-loose",
+                                            dangerous_inner_html: format_args!("<b>{}</b> {} <br />", verse.verse_num, verse.text),
+                                            " "
+                                        }
+                                    }
+                                })
                             }
-                        },
-                        "Previous"
-                    }
-                }
-                div {
-                    class: "fixed bottom-0 right-0 m-4",
-                    button {
-                        class: "px-4 py-2 text-black bg-slate-300 rounded",
-                        onclick: move |_| {
-                            match bible() {
-                                Some(mut curr_bible) => {
-                                    curr_bible.next_chapter();
-                                    current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
-                                    current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
-                                    debug!("{:?}", current_chapter);
-                                    bible.set(Some(curr_bible));
-                                },
-                                None => debug!("Bible match failed")
-                            }
-                        },
-                        "Next"
+                        }
                     }
                 }
             }
+            // div {
+            //     class: "fixed bottom-0 left-250 m-4",
+            //     button {
+            //         class: "px-4 py-2 text-white bg-gray-700 rounded",
+            //         onclick: move |_| {
+            //             match bible() {
+            //                 Some(mut curr_bible) => {
+            //                     curr_bible.previous_chapter();
+            //                     debug!("{:?}", current_chapter);
+            //                     current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
+            //                     current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
+            //                     bible.set(Some(curr_bible));
+            //                 },
+            //                 None => debug!("Bible match failed")
+            //             }
+            //         },
+            //         "Previous"
+            //     }
+            // }
+            // div {
+            //     class: "fixed bottom-0 right-0 m-4",
+            //     button {
+            //         class: "px-4 py-2 text-white bg-gray-700 rounded",
+            //         onclick: move |_| {
+            //             match bible() {
+            //                 Some(mut curr_bible) => {
+            //                     curr_bible.next_chapter();
+            //                     current_chapter_text.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.text.clone()));
+            //                     current_chapter.set(curr_bible.get_current_chapter().map_or("".to_string(), |chapter| chapter.get_pretty_chapter()));
+            //                     debug!("{:?}", current_chapter);
+            //                     bible.set(Some(curr_bible));
+            //                 },
+            //                 None => debug!("Bible match failed")
+            //             }
+            //         },
+            //         "Next"
+            //     }
+            // }
         }
     }
 }
